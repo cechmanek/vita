3eb83d048d2bd4a976781d7d0f7d8d614fd81d5c
justin cechmanek <gj.cechmanek@gmail.com>
2022-12-04 22:43:51 -0700

    figuring out go imports

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
   "fmt"
+  //"core"
+  //"./core"
+  //"vita/core"
+  //"./vita/core"
+  //"core.go"
+  "./core/core.go"
 )
 
 func main() {
   fmt.Println("Hello, world?")
+
+  myfirstpixel := core.IntPixel[1,2,3]
+
+  myfirstpixel.print()
 }
--- pkg/core/core.go
+++ pkg/core/core.go
@@ -1,1 +1,21 @@
 package core
+
+import "fmt"
+
+type Image struct {
+  ( )
+
+type IntPixel struct{
+  values []int
+  length int
+}
+
+type FloatPixel struct{
+  values []float32
+  length int
+}
+
+
+func (p IntPixel) print() {
+  fmt.Println(p.values)
+}
